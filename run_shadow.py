55ca572fa6fe7fab1f2387782fcc231933c579ca
vladimir <vladimir.sokolov789@gmail.com>
2024-05-16 17:42:36 +0200

    Fixed new mistake

--- run_shadow.py
+++ run_shadow.py
@@ -14,7 +14,8 @@ try:
         Polyedr(f"data/{name}.geom").draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
-        print(f"Сумма площадей граней: {Polyedr(f'data/{name}.geom').find_area()}")
+        area = Polyedr(f'data/{name}.geom').find_area()
+        print(f"Сумма площадей граней: {area}")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
